0bf6517cee3019a777101ad9ab701edcc46921b1
Güven Çabat <gvncbt@gmail.com>
2024-03-23 19:19:20 +0300

    deleted unnecessary files

--- Core/Entities/Concrete/OperationClaim.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Core.Entities.Concrete
-{
-    public class OperationClaim : Entity<int>
-    {
-        public string Name { get; set; }
-    }
-}
--- Core/Entities/Concrete/UserOperationClaim.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Core.Entities.Concrete
-{
-    public class UserOperationClaim : Entity<int>
-    {
-        public int UserId { get; set; }
-        public int OperationClaimId { get; set; }
-    }
-}
